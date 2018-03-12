16c53f9e2009aa772b61d3ca3c0e9fd038af0429
An Thanh Nguyen <thanhan@users.noreply.github.com>
2018-03-11 22:05:10 -0500

    del survey

--- survey/update_qual.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-#data = pd.read_csv('Batch_3141946_batch_results.csv')
-data = pd.read_csv('Batch_3145473_batch_results.csv')
-workers = data.WorkerId
-
-update_qual = pd.DataFrame({'Worker ID': workers, 'UPDATE-did fact check survey': 1})
-
-update_qual.to_csv('update_qual.csv')
